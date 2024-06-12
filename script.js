2bcede713e912260a6dbf8c8f9601ac4da949957
vishakh <vishakhhrithi007@gmail.com>
2024-06-13 03:09:20 +0530

    50% completed

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ let hide =document.querySelector('.menu')
 icon.addEventListener('click', function(){
     hide.classList.toggle('is-active')
 });
+var swiper = new Swiper(".mySwiper", {
+    speed:3000,
+    spaceBetween: 0,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // autoplay: {
+    //     delay: 3000,
+    //   },
+  });
